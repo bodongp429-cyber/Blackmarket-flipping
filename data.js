8c441847113d691b2c68dd928665f9b7d5e01a03
bodongp429-cyber <bodongp429@gmail.com>
2026-07-04 14:58:32 +0800

    Add item data for various tiers and enchantments

---
+++ data.js
@@ -0,0 +1,8 @@
+const items = [
+  { tier:"T4", enchant:".0", item:"Soldier Boots", buy:12000, sell:18500 },
+  { tier:"T4", enchant:".1", item:"Soldier Boots", buy:25000, sell:36000 },
+  { tier:"T5", enchant:".0", item:"Cleric Robe", buy:45000, sell:68000 },
+  { tier:"T6", enchant:".2", item:"Battleaxe", buy:180000, sell:265000 },
+  { tier:"T7", enchant:".3", item:"Bear Paws", buy:950000, sell:1350000 },
+  { tier:"T8", enchant:".1", item:"Shadowcaller", buy:1200000, sell:1750000 }
+];
