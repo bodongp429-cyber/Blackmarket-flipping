d6f8293d386d7cdd5810bf6e1922c310fd94734b
bodongp429-cyber <bodongp429@gmail.com>
2026-07-04 14:50:35 +0800

    Implement market item display with profit calculation
    Added functionality to display market items in a table format with profit calculations.

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-console.log("Albion Black Market Flipper Loaded!");
-
 const items = [
   {
     item: "T4 Soldier Boots",
     buy: 12000,
     sell: 18500
+  },
+  {
+    item: "T4 Cleric Robe",
+    buy: 21000,
+    sell: 31500
+  },
+  {
+    item: "T5 Bag",
+    buy: 18000,
+    sell: 25500
+  },
+  {
+    item: "T6 Battleaxe",
+    buy: 85000,
+    sell: 119000
   }
 ];
 
-console.log(items);
+const table = document.getElementById("marketTable");
+
+table.innerHTML = "";
+
+items.forEach(item => {
+
+    const profit = (((item.sell-item.buy)/item.buy)*100).toFixed(1);
+
+    table.innerHTML += `
+        <tr>
+            <td>${item.item}</td>
+            <td>${item.buy.toLocaleString()}</td>
+            <td>${item.sell.toLocaleString()}</td>
+            <td style="color:lime;">+${profit}%</td>
+        </tr>
+    `;
+
+});
