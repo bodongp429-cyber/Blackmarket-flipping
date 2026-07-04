ed05f7b3ea16820fe0dbd5e18b946f52c24903e7
bodongp429-cyber <bodongp429@gmail.com>
2026-07-04 14:56:15 +0800

    Refactor item list and enhance rendering logic

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
 const items = [
-  {
-    item: "T4 Soldier Boots",
-    buy: 12000,
-    sell: 18500
-  },
-  {
-    item: "T4 Cleric Robe",
-    buy: 21000,
-    sell: 31500
-  },
-  {
-    item: "T5 Bag",
-    buy: 18000,
-    sell: 25500
-  },
-  {
-    item: "T6 Battleaxe",
-    buy: 85000,
-    sell: 119000
-  }
+  { tier:"T4", enchant:".0", item:"Soldier Boots", buy:12000, sell:18500 },
+  { tier:"T4", enchant:".1", item:"Soldier Boots", buy:25000, sell:36000 },
+  { tier:"T5", enchant:".0", item:"Cleric Robe", buy:45000, sell:68000 },
+  { tier:"T6", enchant:".2", item:"Battleaxe", buy:180000, sell:265000 },
+  { tier:"T7", enchant:".3", item:"Bear Paws", buy:950000, sell:1350000 },
+  { tier:"T8", enchant:".1", item:"Shadowcaller", buy:1200000, sell:1750000 }
 ];
 
 const table = document.getElementById("marketTable");
+const search = document.getElementById("search");
+const tier = document.getElementById("tier");
+const enchant = document.getElementById("enchant");
 
-table.innerHTML = "";
+function render(){
 
-items.forEach(item => {
+    table.innerHTML="";
 
-    const profit = (((item.sell-item.buy)/item.buy)*100).toFixed(1);
+    let data = items.filter(i=>{
 
-    table.innerHTML += `
+        const cocokNama =
+        i.item.toLowerCase().includes(search.value.toLowerCase());
+
+        const cocokTier =
+        tier.value==="ALL" || i.tier===tier.value;
+
+        const cocokEnchant =
+        enchant.value==="ALL" || i.enchant===enchant.value;
+
+        return cocokNama && cocokTier && cocokEnchant;
+
+    });
+
+    data.sort((a,b)=>{
+
+        const profitA=((a.sell-a.buy)/a.buy);
+        const profitB=((b.sell-b.buy)/b.buy);
+
+        return profitB-profitA;
+
+    });
+
+    data.forEach(i=>{
+
+        const profit=((i.sell-i.buy)/i.buy*100).toFixed(1);
+
+        table.innerHTML += `
         <tr>
-            <td>${item.item}</td>
-            <td>${item.buy.toLocaleString()}</td>
-            <td>${item.sell.toLocaleString()}</td>
-            <td style="color:lime;">+${profit}%</td>
+            <td>${i.tier} ${i.item} ${i.enchant}</td>
+            <td>${i.buy.toLocaleString()}</td>
+            <td>${i.sell.toLocaleString()}</td>
+            <td style="color:lime;">${profit}%</td>
         </tr>
-    `;
+        `;
 
-});
+    });
+
+}
+
+render();
+
+search.oninput=render;
+tier.onchange=render;
+enchant.onchange=render;
